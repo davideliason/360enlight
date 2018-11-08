8defe6850927e8802e7eb798f9bef7e869a0eeca
David Eliason <davethemaker@gmail.com>
2018-11-08 13:47:20 -0800

    add middleware for parsing POSTed form data and logging to console on the server side. Thus, we are ready to take those values and save them to a new collection document

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 8080
+const bodyParser = require('body-parser');
+
 require('dotenv').config();
 
 var data = [
@@ -18,11 +20,21 @@ var data = [
 ]
 
 app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+)
+app.use(bodyParser.json())
 
 
 app.get('/api/users',(req,res,next)=> {
     res.json(data)
    });
+
+app.post('/user/',(req,res) => {
+    console.log(req.body.username);
+})
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
