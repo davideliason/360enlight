1264c0282df391dff2831e8ce6c296ee14686cc5
David Eliason <davethemaker@gmail.com>
2018-11-07 16:55:02 -0800

    add routes so that /api/users has express acting as RESTful API endpoint, captured by /client CRA and rendered

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {users: []}
+
+  componentDidMount() {
+    fetch('/api/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
+
   render() {
     return (
       <div className="App">
          <h3>360enlight</h3>
-         <p>Oh wow!</p>
+         <p>Be present even when you are far away</p>
+        <h1>Users</h1>
+        {this.state.users.map(user =>
+          <div key={user.id}>{user.name} says '{user.text}'</div>
+        )}
       </div>
     );
   }
--- server.js
+++ server.js
@@ -2,15 +2,33 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 8080
+require('dotenv').config();
 
+var data = [
+    {
+        "id" : "001",
+        "name" : "John",
+        "text" : "hello"
+    },
+    {
+        "id" : "002",
+        "name" : "Bob",
+        "text" : "coffee please"
+    },
+]
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', (req,res) => {
-    res.end("hello world");
-});
+
+app.get('/api/users',(req,res,next)=> {
+    res.json(data)
+   });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
 
 app.listen(PORT, () => {
+    console.log(process.env.TEST);
     console.log(`server listening at ${PORT}`);
 });
-
